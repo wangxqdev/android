0e998fdbac3aee0ba7c32be4a82edb8a5573b70d
wangxinquan <wangxinquan@anji-tec.com>
2020-07-21 16:41:26 +0800

    学习相对布局

---
+++ app/src/main/java/com/tec/anji/layout/RelativeLayoutActivity.java
@@ -0,0 +1,14 @@
+package com.tec.anji.layout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.tec.anji.R;
+
+public class RelativeLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative);
+    }
+}
