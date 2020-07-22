18cdbc498a5a4eb056e975b824ce463bccb8482d
wangxinquan <wangxinquan@anji-tec.com>
2020-07-22 16:18:27 +0800

    学习RadioButton

--- app/src/main/java/com/tec/anji/MainActivity.java
+++ app/src/main/java/com/tec/anji/MainActivity.java
@@ -1,11 +1,13 @@
 package com.tec.anji;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import com.tec.anji.view.ButtonActivity;
 import com.tec.anji.view.EditTextActivity;
+import com.tec.anji.view.RadioButtonActivity;
 import com.tec.anji.view.TextViewActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,21 +18,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // TextView
         Button btnTextView = findViewById(R.id.btn_textView);
-        btnTextView.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, TextViewActivity.class);
-            startActivity(intent);
-        });
+        btnTextView.setOnClickListener(view -> startActivity(TextViewActivity.class));
         // Button
         Button btnButton = findViewById(R.id.btn_button);
-        btnButton.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, ButtonActivity.class);
-            startActivity(intent);
-        });
+        btnButton.setOnClickListener(view -> startActivity(ButtonActivity.class));
         // EditText
         Button btnEditText = findViewById(R.id.btn_editText);
-        btnEditText.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, EditTextActivity.class);
-            startActivity(intent);
-        });
+        btnEditText.setOnClickListener(view -> startActivity(EditTextActivity.class));
+        // RadioButton
+        Button btnRadioButton = findViewById(R.id.btn_radioButton);
+        btnRadioButton.setOnClickListener(view -> startActivity(RadioButtonActivity.class));
+    }
+
+    private void startActivity(Class<? extends Context> cls) {
+        Intent intent = new Intent(MainActivity.this, cls);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/tec/anji/view/RadioButtonActivity.java
@@ -0,0 +1,23 @@
+package com.tec.anji.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import com.tec.anji.R;
+
+public class RadioButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radio_button);
+        // 事件监听
+        RadioGroup rg2 = findViewById(R.id.rg_2);
+        rg2.setOnCheckedChangeListener((group, checkedId) -> {
+            RadioButton radioButton = findViewById(checkedId);
+            Toast.makeText(RadioButtonActivity.this, radioButton.getText().toString(), Toast.LENGTH_SHORT).show();
+        });
+    }
+}
